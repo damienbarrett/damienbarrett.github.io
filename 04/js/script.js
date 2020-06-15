4674d57d7a2002c2cf5e717030e9c59dc7e81658
Damien Barrett <damienbarrett@Damiens-MBP.lan>
2020-06-16 08:05:08 +1200

    Added form.

---
+++ 04/js/script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    alert("Hello, world!");
+}
+
+$('#file-upload').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename').html(filename);
+
+});
